843d3faeb3699397db938945ef4bef6232f7fbaa
Satori32 <Huga8989+githubf@gmail.com>
2024-10-14 19:40:13 +0900

    bug fix

--- ソース.cpp
+++ ソース.cpp
@@ -56,7 +56,7 @@ bool Push(Vector<T>& In, const T& D) {
 	return true;
 }
 template<class T>
-bool Clear(Vector<T> In) {
+bool Clear(Vector<T>& In) {
 	if (Free(In.M) == false) { return false; };
 	In.Capacity = 0;
 	In.Margin = 0;
