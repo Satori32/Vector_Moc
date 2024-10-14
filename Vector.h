b375d5f7790b4763dcaf76ccfed4e72db7c24db2
Satori32 <Huga8989+githubf@gmail.com>
2024-10-14 19:35:35 +0900

    add header text

--- Vector.h
+++ Vector.h
@@ -25,3 +25,4 @@ template <class T> size_t Capacity(Vector<T>& In);
 template <class T> bool Drop(Vector<T>& In, size_t P);
 template <class T> bool Drop(Vector<T>& In, size_t P, size_t L);
 template <class T> bool Free(Vector<T>& In);
+template <class T> bool Clear(Vector<T>& In);
